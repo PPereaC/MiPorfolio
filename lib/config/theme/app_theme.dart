8ecb8ab3ae0ce92abe0ee74deffc96ca82586e6d
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-02-12 20:35:03 +0100

    add: Tema de colores + foto de perfil

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  AppTheme();
+
+  ColorScheme _getColorScheme() {
+    final colors = ColorScheme(
+      brightness: Brightness.dark,
+      primary: const Color(0xFFD7F3FF),
+      secondary: const Color(0xFFe29e21), 
+      surface: const Color(0xFF17202a),
+      error: Colors.red[400]!,
+      onPrimary: Colors.black,
+      onSecondary: Colors.black,
+      onSurface: const Color(0xFF434c57),
+      onError: Colors.white,
+    );
+
+    return colors;
+  }
+
+  ThemeData getTheme() {
+    final colorScheme = _getColorScheme();
+
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: colorScheme,
+      fontFamily: 'Comfortaa',
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 50, 
+          fontWeight: FontWeight.bold, 
+          color: Colors.white,
+          fontFamily: 'Arial',
+        ),
+        titleMedium: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white),
+        titleSmall: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+        bodyLarge: TextStyle(fontSize: 20, color: Colors.white),
+        bodyMedium: TextStyle(fontSize: 14, color: Colors.white),
+        bodySmall: TextStyle(fontSize: 12, color: Colors.white),
+        labelLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white),
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: colorScheme.surface,
+        foregroundColor: Colors.white,
+        centerTitle: false,
+      ),
+    );
+  }
+  
+}
