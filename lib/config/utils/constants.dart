6d9ec25bec9334d831b10316e3322f8af657d692
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-02-12 20:33:32 +0100

    add: Enlaces sociales

--- lib/config/utils/constants.dart
+++ lib/config/utils/constants.dart
@@ -1,2 +1,7 @@
 const mobileWidth = 600;
+
+// Links
 const linkedinUrl = 'https://www.linkedin.com/in/pablopereacampos/';
+const githubLink = 'https://github.com/PPereaC';
+const emailLink = 'mailto:pereacampospablo@gmail.com?subject=Contacto%20desde%20Portfolio';
+const cvLink = 'https://app.filen.io/#/d/38d7f23c-1a13-4937-bab6-12659fa24921#1BjywEjwVs8qEPMT0qvJkqmmY0OXcXsw';
