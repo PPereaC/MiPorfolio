9f3d15c02386d5439367ac044d5b5a672ef70bb8
Pablo Perea Campos <74180013+PPereaC@users.noreply.github.com>
2025-02-19 20:51:37 +0100

    update: Seccion introduccion para movil

--- lib/presentation/responsive/mobile_body.dart
+++ lib/presentation/responsive/mobile_body.dart
@@ -1,27 +1,94 @@
 import 'package:flutter/material.dart';
 
+import '../sections/sections.dart';
+import '../widgets/widgets.dart';
+
 class MobileBody extends StatelessWidget {
   const MobileBody({super.key});
 
   @override
   Widget build(BuildContext context) {
 
+    final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    return CustomScrollView(
-      slivers: [
-        // Sección de introducción
-        const SliverToBoxAdapter(
-          child: SizedBox(
+    return Stack(
+      children: [
+
+        // Secciones
+        CustomScrollView(
+          slivers: [
             
-          ),
+            // Sección de introducción
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+                child: const IntroductionSection(),
+              ),
+            ),
+        
+            // // Sección de los proyectos
+            // SliverToBoxAdapter(
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            //     child: const ProjectsSection(),
+            //   ),
+            // ),
+        
+            // SliverToBoxAdapter(
+            //   child: SizedBox(height: height * 0.15),
+            // ),
+        
+            // // Sección de la experiencia
+            // SliverToBoxAdapter(
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            //     child: const ExperienceSection(),
+            //   ),
+            // ),
+        
+            // SliverToBoxAdapter(
+            //   child: SizedBox(height: height * 0.1),
+            // ),
+        
+            // // Sección de habilidades
+            // SliverToBoxAdapter(
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            //     child: const SkillsSection(),
+            //   ),
+            // ),
+        
+            // // Sección de formación
+            // SliverToBoxAdapter(
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            //     child: const TrainingSection(),
+            //   ),
+            // ),
+        
+            // SliverToBoxAdapter(
+            //   child: SizedBox(height: height * 0.15),
+            // ),
+        
+            // // Sección de footer
+            // SliverToBoxAdapter(
+            //   child: Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            //     child: const FooterSection(),
+            //   ),
+            // ),
+        
+          ],
         ),
-        SliverToBoxAdapter(
-          child: SizedBox(height: height * 0.05),
-        ),
-        const SliverToBoxAdapter(
-          child: SizedBox(height: 28),
-        ),
+
+        // // Selector de idioma flotante
+        // const Positioned(
+        //   top: 20,
+        //   right: 20,
+        //   child: LanguagueSelect(),
+        // ),
+
       ],
     );
   }
